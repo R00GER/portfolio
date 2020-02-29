22073da5534e6288822ba671ff3db73e202adb6e
R00GER <roni.friman@sonit.fi>
2020-02-29 12:22:12 +0200

    Add existing project

---
+++ js/script.js
@@ -0,0 +1,164 @@
+let elements = {
+    parentElements: {
+        nav: document.querySelector(".navbar")
+    },
+
+    clickableElements: {
+        nav_home: document.querySelector("#nav_home"),
+        nav_projects: document.querySelector("#nav_projects"),
+        nav_studies: document.querySelector("#nav_studies"),
+        nav_work: document.querySelector("#nav_work"),
+        nav_cv: document.querySelector("#nav_cv"),
+        pager_home: document.querySelector("#pager_home"),
+        pager_projects: document.querySelector("#pager_projects"),
+        pager_studies: document.querySelector("#pager_studies"),
+        pager_work: document.querySelector("#pager_work"),
+        pager_cv: document.querySelector("#pager_cv"),
+        arrow_projects: document.querySelector("#arrow-1"),
+        arrow_studies: document.querySelector("#arrow-2"),
+        arrow_work: document.querySelector("#arrow-3"),
+        arrow_cv: document.querySelector("#arrow-4"),
+        arrow_backToHome: document.querySelector("#arrow-5")
+    },
+
+    pages: {
+        home: document.querySelector("#page_home"),
+        projects: document.querySelector("#page_projects"),
+        studies: document.querySelector("#page_studies"),
+        work: document.querySelector("#page_work"),
+        cv: document.querySelector("#page_cv"),
+        pager_home: document.querySelector("#page_home"),
+        pager_projects: document.querySelector("#page_projects"),
+        pager_studies: document.querySelector("#page_studies"),
+        pager_work: document.querySelector("#page_work"),
+        pager_cv: document.querySelector("#page_cv"),
+        arrow_projects: document.querySelector("#page_projects"),
+        arrow_studies: document.querySelector("#page_studies"),
+        arrow_work: document.querySelector("#page_work"),
+        arrow_cv: document.querySelector("#page_cv"),
+        arrow_backToHome: document.querySelector("#page_home"),
+    },
+
+    getActiveElements: function() {
+
+        let activeNavElement = elements.clickableElements.nav_home;
+        let activePagerElement = elements.clickableElements.pager_home;
+
+        const homePos = elements.pages.home.offsetTop + (window.innerHeight * 0.6);
+        const studiesPos = elements.pages.studies.offsetTop + (window.innerHeight * 0.6);
+        const workPos = elements.pages.work.offsetTop + (window.innerHeight * 0.6);
+        const cvPos = elements.pages.cv.offsetTop + (window.innerHeight * 0.6);
+        const projectsPos = elements.pages.projects.offsetTop + (window.innerHeight * 0.6);
+
+        // const projectsPos = elements.pages.projects.offsetTop + (window.innerHeight * 1.5);
+
+        if (window.pageYOffset < homePos) {
+            activePage = elements.pages.home;
+            activeNavElement.classList.add("active-nav-link");
+            activePagerElement.classList.add("active-pager");
+            elements.clickableElements.nav_projects.classList.remove("active-nav-link");
+            elements.clickableElements.pager_projects.classList.remove("active-pager");
+
+        } else if (window.pageYOffset < projectsPos) {
+            // LET PREVACTIVEELEMENTS = GETELEMENTBYCLASSNAME("ACTIVE-NAV-LINK, ACTIVE-PAGER")
+            // NEW ACTIVE ELEMENTS
+            // PASS PREV AND NEW ELEMENTS TO FUNCTION
+            // FUNCTION TO REMOVE AND ADD ACTIVE CLASSES
+
+            // activeNavElement.classList.remove("active-nav-link");
+            // activePagerElement.classList.remove("active-pager");
+            elements.clickableElements.nav_home.classList.remove("active-nav-link");
+            elements.clickableElements.pager_home.classList.remove("active-pager");
+            activePage = elements.pages.projects;
+            activeNavElement = elements.clickableElements.nav_projects;
+            activePagerElement = elements.clickableElements.pager_projects;
+            activeNavElement.classList.add("active-nav-link");
+            activePagerElement.classList.add("active-pager");
+            elements.clickableElements.nav_studies.classList.remove("active-nav-link");
+            elements.clickableElements.pager_studies.classList.remove("active-pager");
+
+        } else if (window.pageYOffset < studiesPos) {
+            elements.clickableElements.nav_projects.classList.remove("active-nav-link");
+            elements.clickableElements.pager_projects.classList.remove("active-pager");
+            activePage = elements.pages.studies;
+            activeNavElement = elements.clickableElements.nav_studies;
+            activePagerElement = elements.clickableElements.pager_studies;
+            activeNavElement.classList.add("active-nav-link");
+            activePagerElement.classList.add("active-pager");
+            elements.clickableElements.nav_work.classList.remove("active-nav-link");
+            elements.clickableElements.pager_work.classList.remove("active-pager");
+
+        } else if (window.pageYOffset < workPos) {
+            elements.clickableElements.nav_studies.classList.remove("active-nav-link");
+            elements.clickableElements.pager_studies.classList.remove("active-pager");
+            activePage = elements.pages.work;
+            activeNavElement = elements.clickableElements.nav_work;
+            activePagerElement = elements.clickableElements.pager_work;
+            activeNavElement.classList.add("active-nav-link");
+            activePagerElement.classList.add("active-pager");
+            elements.clickableElements.nav_cv.classList.remove("active-nav-link");
+            elements.clickableElements.pager_cv.classList.remove("active-pager");
+
+        } else if (window.pageYOffset < cvPos) {
+            elements.clickableElements.nav_work.classList.remove("active-nav-link");
+            elements.clickableElements.pager_work.classList.remove("active-pager");
+            activePage = elements.pages.cv;
+            activeNavElement = elements.clickableElements.nav_cv;
+            activePagerElement = elements.clickableElements.pager_cv;
+            activeNavElement.classList.add("active-nav-link");
+            activePagerElement.classList.add("active-pager");
+        }
+    }
+}
+
+document.addEventListener("click", scrollToElement);
+
+function scrollToElement() {
+    let element = event.target;
+    console.log(element.tagName);
+
+    if (element.tagName === "A" || element.tagName === "I") {
+        const clickableElementsArr = Object.values(elements.clickableElements);
+        const pagesArr = Object.values(elements.pages);
+        let clickedElementIndex = clickableElementsArr.indexOf(element);
+        element = pagesArr[clickedElementIndex];
+
+        element.scrollIntoView({
+            behavior: "smooth"
+        });
+
+    } else {
+        return;
+    }
+}
+
+function navColor() {
+    let nav = elements.parentElements.nav;
+    let offset = 0.92;
+    let home = elements.pages.home.offsetHeight * offset;
+
+    if (window.pageYOffset >= home) {
+        nav.style.backgroundColor = "rgba(48, 46, 46, 0.7)";
+    } else {
+        nav.style.backgroundColor = "";
+    }
+}
+
+let tempPosition = window.pageYOffset;
+
+function hideNav() {
+    position = window.pageYOffset;
+    if (tempPosition > position) {
+        document.querySelector(".navbar").style.top = "0";
+    } else {
+        document.querySelector(".navbar").style.top = "-5vh";
+    }
+    tempPosition = position;
+}
+
+window.onscroll = function() {
+    let currentScrollPos = window.pageYOffset;
+    elements.getActiveElements();
+    navColor();
+    hideNav();
+}
---
+++ js/typed.js
@@ -0,0 +1,8 @@
+var typed = new Typed('.title', {
+    strings: ["HELLO", "THIS IS MY", "PORTFPLO", "PORTFOLIO"],
+    startDelay: 2900,
+    backSpeed: 100,
+    typeSpeed: 70,
+    backDelay: 200,
+    showCursor: false
+});
